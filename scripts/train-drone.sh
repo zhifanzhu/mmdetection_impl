328db67044a44675cd908e71566bcddd5d043983
ktw361 <zhifanzhu@yeah.net>
2019-05-28 17:08:35 +0800

    modify train-drone.sh

--- scripts/train-drone.sh
+++ scripts/train-drone.sh
@@ -1,18 +1,19 @@
-MMDET=/home/damon/Github/mmlab/mmdetection
+MMDET=$HOME/Github/mmlab/mmdetection
 export PYTHONPATH=$PYTHONPATH:$MMDET
 
 CURDIR=`dirname "$0"`
 CURDIR=`realpath $CURDIR`
 CONFIG_FILE=`ls $CURDIR/*.py`
+LOGFILE=$CURDIR/log.txt
 # Please set work dir in config file
 
 CHECKPOINT_FILE=$CURDIR/latest.pth
 if [ -f "$CHECKPOINT_FILE" ]; then
     echo "train-drone.sh: Found checkpoint file"
     cd $MMDET
-    python $MMDET/tools/train.py ${CONFIG_FILE} --resume_from $CHECKPOINT_FILE
+    python $MMDET/tools/train.py ${CONFIG_FILE} --resume_from $CHECKPOINT_FILE | tee $LOGFILE
 else
     echo "train-drone.sh: No checkpoint, fresh start"
     cd $MMDET
-    python $MMDET/tools/train.py ${CONFIG_FILE}
+    python $MMDET/tools/train.py ${CONFIG_FILE} | tee $LOGFILE
 fi
