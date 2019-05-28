eb42d16cd4cab488a819ab31e12db8b3c490d16f
ktw361 <zhifanzhu@yeah.net>
2019-05-28 19:06:25 +0800

    update test-drone.sh

--- scripts/test-drone.sh
+++ scripts/test-drone.sh
@@ -1,8 +1,15 @@
-CONFIG_FILE=visdrone/configs/ssd300.py
-CHECKPOINT_FILE=work_dirs/ssd300_visdrone/latest.pth
-RESULT_FILE=work_dirs/ssd300_visdrone/result.pkl
+MMDET=/home/damon/Github/mmlab/mmdetection
+export PYTHONPATH=$PYTHONPATH:$MMDET
+
+CURDIR=`dirname "$0"`
+CURDIR=`realpath $CURDIR`
+CONFIG_FILE=`ls $CURDIR/*.py`
+
+CHECKPOINT_FILE=$CURDIR/latest.pth
+RESULT_FILE=$CURDIR/result.pkl
 EVAL_METRICS=bbox
 
 # single-gpu testing
+cd $MMDET
 python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --out ${RESULT_FILE} --eval ${EVAL_METRICS}
 # python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --out ${RESULT_FILE} --eval ${EVAL_METRICS} --show
