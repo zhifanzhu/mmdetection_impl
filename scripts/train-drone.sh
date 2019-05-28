2f231d6cb88c301db84c7bfa0917319e65dc9deb
ktw361 <zhifanzhu@yeah.net>
2019-05-28 17:10:09 +0800

    modify train-drone.sh

--- scripts/train-drone.sh
+++ scripts/train-drone.sh
@@ -11,9 +11,9 @@ CHECKPOINT_FILE=$CURDIR/latest.pth
 if [ -f "$CHECKPOINT_FILE" ]; then
     echo "train-drone.sh: Found checkpoint file"
     cd $MMDET
-    python $MMDET/tools/train.py ${CONFIG_FILE} --resume_from $CHECKPOINT_FILE | tee $LOGFILE
+    python $MMDET/tools/train.py ${CONFIG_FILE} --resume_from $CHECKPOINT_FILE 2>&1 | tee -a $LOGFILE
 else
     echo "train-drone.sh: No checkpoint, fresh start"
     cd $MMDET
-    python $MMDET/tools/train.py ${CONFIG_FILE} | tee $LOGFILE
+    python $MMDET/tools/train.py ${CONFIG_FILE} 2>&1 | tee -a $LOGFILE
 fi
