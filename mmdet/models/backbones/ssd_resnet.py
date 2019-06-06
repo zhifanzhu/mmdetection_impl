aecdfec91e48ca210e21835aee35555640f1b14f
ktw361 <zhifanzhu@yeah.net>
2019-06-06 11:39:29 +0800

    SSDResNet atrous mode

--- mmdet/models/backbones/ssd_resnet.py
+++ mmdet/models/backbones/ssd_resnet.py
@@ -27,6 +27,10 @@ With 512 input, output feature map shape:
      torch.Size([1, 256, 4, 4]),
      torch.Size([1, 256, 2, 2]),
      torch.Size([1, 128, 1, 1])]
+     
+If C3 is also in atrous mode:
+    out_from=('layer3', '', '', '', '', '', ''),
+    out_channels=(-1, 512, 512, 256, 256, 128, 128),
 
 plain conv in extra layer:
     Forward/backward pass size (MB): 2460.58
@@ -36,6 +40,10 @@ use BasicBlock in extra layer:
     Params size (MB): 243.98
 '''
 
+STAGES_DEFAULT = (1, 2, 2, 2)
+DILATIONS_DEFAULT = (1, 1, 1, 1)
+OUT_INDICES_DEFAULT = (0, 1, 2, 3)
+STAGE_WITH_DCN_DEFAULT = (False, False, False, False)
 
 @BACKBONES.register_module
 class SSDResNet(ResNet):
@@ -43,11 +51,9 @@ class SSDResNet(ResNet):
     def __init__(self,
                  depth=101,
                  num_stages=3,
-                 strides=(1, 2, 2),
-                 dilations=(1, 1, 1),
-                 out_indices=(0, 1, 2),
                  out_from=('layer3', '', '', '', '', ''),
                  out_channels=(-1, 512, 512, 256, 256, 128),
+                 use_dilation_conv4=False,
                  use_dilation_conv5=True,
                  use_resblock_in_extra=False,
                  style='pytorch',
@@ -56,36 +62,48 @@ class SSDResNet(ResNet):
                  norm_cfg=dict(type='BN', requires_grad=True),
                  norm_eval=True,
                  dcn=None,
-                 stage_with_dcn=(False, False, False),
                  with_cp=False,
                  zero_init_residual=True):
         super(SSDResNet, self).__init__(
             depth,
             num_stages,
-            strides,
-            dilations,
-            out_indices,
+            STAGES_DEFAULT[:num_stages],
+            DILATIONS_DEFAULT[:num_stages],
+            OUT_INDICES_DEFAULT[:num_stages],
             style,
             frozen_stages,
             conv_cfg,
             norm_cfg,
             norm_eval,
             dcn,
-            stage_with_dcn,
+            STAGE_WITH_DCN_DEFAULT[:num_stages],
             with_cp,
             zero_init_residual)
 
         assert len(out_channels) == len(out_from)
+        if use_dilation_conv4:
+            assert num_stages == 2 and use_dilation_conv5, \
+            'if use atrous on conv4_1, num_stages should be 2'
+            assert len(out_channels) == 7, \
+            'if use atrous on conv4_1, need to indicate one more feature map'
+        else:
+            if use_dilation_conv5:
+                assert num_stages == 3, \
+                'if use atrous on conv5_1, num_stages should be 3'
+            else:
+                assert num_stages == 4, \
+                'if not using atrous, num_stages should be 4'
+
         self.use_resblock_in_extra = use_resblock_in_extra
         self.out_from = out_from
         self.out_channels = out_channels
 
-        # 1. add atrous conv as C5
-        inplanes = self.feat_dim
-        planes = 64 * 2 ** num_stages
-        num_blocks = 3
         dcn = self.dcn if self.stage_with_dcn[-1] else None
-        if use_dilation_conv5:
+        # 1. add atrous convs to C4
+        if use_dilation_conv4:
+            inplanes = self.feat_dim
+            planes = 64 * 2 ** num_stages
+            num_blocks = 23  # see arch_setting
             stride = 1
             dilation_first = 2
             dilation_other = 1
@@ -103,27 +121,40 @@ class SSDResNet(ResNet):
                 norm_cfg=norm_cfg,
                 dcn=dcn
             )
-        else:
-            stride = 2
-            dilation = 1
-            res_layer = make_res_layer(
+            layer_name = 'layer{}'.format(num_stages + 1)
+            self.add_module(layer_name, res_layer)
+            self.res_layers.append(layer_name)
+            self.feat_dim = self.block.expansion * 64 * 2**(num_stages)
+            num_stages += 1
+
+        # 2. add atrous conv as C5
+        if use_dilation_conv5:
+            inplanes = self.feat_dim
+            planes = 64 * 2 ** num_stages
+            num_blocks = 3  # see arch_setting
+            stride = 1
+            dilation_first = 2
+            dilation_other = 1
+            res_layer = _make_res_atrous_layer(
                 self.block,
                 inplanes,
                 planes,
                 blocks=num_blocks,
                 stride=stride,
-                dilation=dilation,
+                dilation_first=dilation_first,
+                dilation_other=dilation_other,
                 style=style,
                 with_cp=with_cp,
                 conv_cfg=conv_cfg,
                 norm_cfg=norm_cfg,
                 dcn=dcn
             )
-        layer_name = 'layer{}'.format(num_stages + 1)
-        self.add_module(layer_name, res_layer)
-        self.res_layers.append(layer_name)
+            layer_name = 'layer{}'.format(num_stages + 1)
+            self.add_module(layer_name, res_layer)
+            self.res_layers.append(layer_name)
+            self.feat_dim = self.block.expansion * 64 * 2**(num_stages)
 
-        # 2. add extra layers
+        # 3. add extra layers
         self.out_channels = out_channels
         self.extra = self._make_extra_layers(self.out_channels)
 
@@ -175,7 +206,8 @@ class SSDResNet(ResNet):
         return tuple(outs)
 
     def _make_extra_layers(self, outplanes):
-        inplanes = self.block.expansion * 64 * 2 ** self.num_stages  # 2048 if num_stages = 3
+        inplanes = self.feat_dim
+        # inplanes = self.block.expansion * 64 * 2 ** self.num_stages  # 2048 if num_stages = 3
         layers = []
         for planes in outplanes:
             if planes == -1:
