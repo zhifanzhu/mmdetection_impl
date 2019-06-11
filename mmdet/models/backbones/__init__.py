eefb2122a3fe0ce58423bbc2dd3c9a6d5dfaf7ea
ktw361 <zhifanzhu@yeah.net>
2019-06-11 16:18:11 +0800

    add ssd vgg volatile: move anchor feature map down a level

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -1,7 +1,9 @@
 from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
+from .ssd_vgg_volatile import SSDVGG_VOLATILE
 from .ssd_resnet import SSDResNet
 from .hrnet import HRNet
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'SSDResNet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'SSDResNet',
+           'SSDVGG_VOLATILE']
