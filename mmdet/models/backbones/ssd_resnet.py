a7208c1507491c93a66b8033bc19c15dc07c0ce0
ktw361 <zhifanzhu@yeah.net>
2019-06-19 11:38:59 +0800

    add ssd512_r50 more options

--- mmdet/models/backbones/ssd_resnet.py
+++ mmdet/models/backbones/ssd_resnet.py
@@ -39,6 +39,28 @@ plain conv in extra layer:
 use BasicBlock in extra layer:
     Forward/backward pass size (MB): 2469.61
     Params size (MB): 243.98
+
+The backbone has one of these settings:
+    1) num_stages=2, use_dilation_conv4=True, use_dilation_conv5=True:
+        C1 - C2 - C3 - C4_atours - C5_atrous
+
+    2) num_stages=3, use_dilation_conv4=False, use_dilation_conv5=True:
+        C1 - C2 - C3 - C4 - C5_atrous
+
+    3) num_stages=3, use_dilation_conv4=False, use_dilation_conv5=False:
+        C1 - C2 - C3 - C4
+
+    4) (not recommended) num_stages=4, use_dilation_conv4=False,
+        use_dilation_conv5=False:
+        C1 - C2 - C3 - C4 - C5
+
+    5) (not recommended ?) num_stages=4, use_dilation_conv4=True,
+        use_dilation_conv5=False:
+        C1 - C2 - C3 - C4_atours
+Other settings will not be allowed.
+
+use_dilation_conv4 controls the type of Conv for C4,
+while use_dilation_conv5 controls the PRESENCE of C5.
 '''
 
 STAGES_DEFAULT = (1, 2, 2, 2)
@@ -55,6 +77,7 @@ class SSDResNet(ResNet):
                  out_from=('layer3', '', '', '', '', ''),
                  out_channels=(-1, 512, 512, 256, 256, 128),
                  use_dilation_conv4=False,
+                 keep_conv5=True,
                  use_dilation_conv5=True,
                  use_resblock_in_extra=False,
                  style='pytorch',
@@ -65,6 +88,30 @@ class SSDResNet(ResNet):
                  dcn=None,
                  with_cp=False,
                  zero_init_residual=True):
+        '''
+        :param depth: type of ResNet
+        :param num_stages: num_stages to use from Classification ResNet
+        :param out_from:  layer from which generates feature map.
+        :param out_channels: #-channels of out, this should match 'out_from'
+        :param use_dilation_conv4: when set, use_dilation_conv5 must be True
+            and num_stages must be 2.
+        :param use_dilation_conv5: When set to True, use atrous Conv5,
+            otherwise no C5 layer will NOT be added.
+        :param use_resblock_in_extra: use resblock in extra conv.
+        :param style (str): `pytorch` or `caffe`. If set to "pytorch", the stride-two
+            layer is the 3x3 conv layer, otherwise the stride-two layer is
+            the first 1x1 conv layer.
+        :param frozen_stages (int): Stages to be frozen (stop grad and set eval mode).
+            -1 means not freezing any parameters.
+        :param norm_cfg (dict): dictionary to construct and config norm layer.
+        :param norm_eval (bool): Whether to set norm layers to eval mode, namely,
+            freeze running stats (mean and var). Note: Effect on Batch Norm
+            and its variants only.
+        :param with_cp (bool): Use checkpoint or not. Using checkpoint will save some
+            memory while slowing down the training speed.
+        :param zero_init_residual (bool): whether to use zero init for last norm layer
+            in resblocks to let them behave as identity.
+        '''
         super(SSDResNet, self).__init__(
             depth=depth,
             num_stages=num_stages,
@@ -83,17 +130,14 @@ class SSDResNet(ResNet):
 
         assert len(out_channels) == len(out_from)
         if use_dilation_conv4:
-            assert num_stages == 2 and use_dilation_conv5, \
+            assert num_stages == 2, \
                 'if use atrous on conv4_1, num_stages should be 2'
             assert len(out_channels) == 7, \
                 'if use atrous on conv4_1, need to indicate one more feature map'
         else:
             if use_dilation_conv5:
                 assert num_stages == 3, \
-                    'if use atrous on conv5_1, num_stages should be 3'
-            else:
-                assert num_stages == 4, \
-                    'if not using atrous, num_stages should be 4'
+                    'if use atrous only on conv5_1, num_stages should be 3'
 
         self.use_resblock_in_extra = use_resblock_in_extra
         self.out_from = out_from
@@ -129,7 +173,7 @@ class SSDResNet(ResNet):
             num_stages += 1
 
         # 2. add atrous conv as C5
-        if use_dilation_conv5:
+        if keep_conv5 and use_dilation_conv5:
             inplanes = self.feat_dim
             planes = 64 * 2 ** num_stages
             num_blocks = self.arch_settings[self.depth][1][num_stages]
@@ -153,7 +197,7 @@ class SSDResNet(ResNet):
             layer_name = 'layer{}'.format(num_stages + 1)
             self.add_module(layer_name, res_layer)
             self.res_layers.append(layer_name)
-            self.feat_dim = self.block.expansion * 64 * 2**(num_stages)
+            self.feat_dim = self.block.expansion * 64 * 2 ** num_stages
 
         # 3. add extra layers
         self.out_channels = out_channels
