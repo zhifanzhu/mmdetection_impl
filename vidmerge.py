012ef4830853933062a4f94d5584902de99a17c2
Zhifan Zhu <zhuzhifan@yeah.net>
2019-07-08 21:57:14 +0800

    fix vidmerge

--- vidmerge.py
+++ vidmerge.py
@@ -4,9 +4,8 @@ from visdrone.utils import result_utils
 from visdrone.utils import MergeTxt
 
 SEQ_LIST = [
-    '/tmp/testpatch_ga6401500',
-    '/tmp/testpatch_A36401500Real',
-    '/tmp/testpatch_libraLowThr6401500',
+    '~/val896_A2enhance',
+    '/tmp/valori_gavidseq/',
 ]
 
 print(SEQ_LIST)
@@ -15,6 +14,7 @@ print(nms_param)
 
 
 # 1 split seq into images
+SEQ_LIST = [osp.expanduser(v) for v in SEQ_LIST]
 seq_ds = [result_utils.single_seq2res(v) for v in SEQ_LIST]
 # 2 merge images
 d = MergeTxt.merge_dicts(seq_ds, nms_param)
@@ -28,13 +28,12 @@ def dict2seq(d, save_dir):
     tmp_dict = dict()
     for img_name, img_dets in d.items():
         info = img_name.split('_')
-        seq_name = info[:-1]
+        seq_name = '_'.join(info[:-1])
         frame_ind = int(info[-1])
         if seq_name not in tmp_dict:
             tmp_dict[seq_name] = {frame_ind: img_dets}
         else:
-            seq_name[seq_name][frame_ind] = img_dets
-    MergeTxt.save_seq_results(save_dir, tmp_dict)
-
+            tmp_dict[seq_name][frame_ind] = img_dets
+    MergeTxt.save_seq_results(tmp_dict, save_dir)
 
 dict2seq(d, out_dir)
