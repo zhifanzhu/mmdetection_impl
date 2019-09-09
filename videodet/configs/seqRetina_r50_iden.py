9c1818e873948fe02f4d5525facbdb946b33ec3e
ktw361 <zhifanzhu@yeah.net>
2019-09-09 10:08:36 +0800

    update config

--- videodet/configs/seqRetina_r50_iden.py
+++ videodet/configs/seqRetina_r50_iden.py
@@ -61,7 +61,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='LoadAnnotations', with_bbox=True, skip_img_without_anno=False),
     dict(type='Resize', img_scale=(512, 512), keep_ratio=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
