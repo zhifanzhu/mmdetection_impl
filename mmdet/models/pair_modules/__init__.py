641964341902eaf6bda6c65054e2c2779579f110
Zhifan Zhu <zhifanzhu@yeah.net>
2020-04-29 20:27:49 +0800

    pair nonlocal

--- mmdet/models/pair_modules/__init__.py
+++ mmdet/models/pair_modules/__init__.py
@@ -2,6 +2,7 @@ from .identity import Identity
 from .embed_64 import Embed64
 from .corr_assemble import CorrAssemble, MultiCorrAssemble
 from .pycorr_assemble import PyCorrAssemble
+from .pair_nonlocal import PairNonLocal
 
 from .twin_grab import TwinGrab
 from .twin_direct import TwinDirect
@@ -10,6 +11,6 @@ from .twin_nonlocal import TwinNonLocal
 __all__ = [
     'Identity',
     'CorrAssemble', 'MultiCorrAssemble',
-    'PyCorrAssemble', 'Embed64',
+    'PyCorrAssemble', 'Embed64', 'PairNonLocal',
     'TwinGrab', 'TwinDirect', 'TwinNonLocal',
 ]
