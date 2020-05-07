f75e4e0cecc7e5e61f471d84fd4a3f750366100a
Zhifan Zhu <zhifanzhu@yeah.net>
2020-05-07 23:42:45 +0800

    new embed ca (disp=1), in all layers

--- mmdet/models/pair_modules/twin_embed_ca.py
+++ mmdet/models/pair_modules/twin_embed_ca.py
@@ -7,116 +7,6 @@ from ..registry import PAIR_MODULE
 from ..utils import ConvModule
 
 
-class NonLocal2D(nn.Module):
-    """Non-local module.
-
-    See https://arxiv.org/abs/1711.07971 for details.
-
-    Args:
-        in_channels (int): Channels of the input feature map.
-        reduction (int): Channel reduction ratio.
-        use_scale (bool): Whether to scale pairwise_weight by 1/inter_channels.
-        conv_cfg (dict): The config dict for convolution layers.
-            (only applicable to conv_out)
-        norm_cfg (dict): The config dict for normalization layers.
-            (only applicable to conv_out)
-        mode (str): Options are `embedded_gaussian` and `dot_product`.
-    """
-
-    def __init__(self,
-                 in_channels,
-                 reduction=2,
-                 use_scale=True,
-                 conv_cfg=None,
-                 norm_cfg=None,
-                 mode='embedded_gaussian'):
-        super(NonLocal2D, self).__init__()
-        self.in_channels = in_channels
-        self.reduction = reduction
-        self.use_scale = use_scale
-        self.inter_channels = in_channels // reduction
-        self.mode = mode
-        assert mode in ['embedded_gaussian', 'dot_product']
-
-        # g, theta, phi are actually `nn.Conv2d`. Here we use ConvModule for
-        # potential usage.
-        self.g = ConvModule(
-            self.in_channels,
-            self.inter_channels,
-            kernel_size=1,
-            activation=None)
-        self.theta = ConvModule(
-            self.in_channels,
-            self.inter_channels,
-            kernel_size=1,
-            activation=None)
-        self.phi = ConvModule(
-            self.in_channels,
-            self.inter_channels,
-            kernel_size=1,
-            activation=None)
-        self.conv_out = ConvModule(
-            self.inter_channels,
-            self.in_channels,
-            kernel_size=1,
-            conv_cfg=conv_cfg,
-            norm_cfg=norm_cfg,
-            activation=None)
-
-        self.init_weights()
-
-    def init_weights(self, std=0.01, zeros_init=True):
-        for m in [self.g, self.theta, self.phi]:
-            normal_init(m.conv, std=std)
-        if zeros_init:
-            constant_init(self.conv_out.conv, 0)
-        else:
-            normal_init(self.conv_out.conv, std=std)
-
-    def embedded_gaussian(self, theta_x, phi_x):
-        # pairwise_weight: [N, HxW, HxW]
-        pairwise_weight = torch.matmul(theta_x, phi_x)
-        if self.use_scale:
-            # theta_x.shape[-1] is `self.inter_channels`
-            pairwise_weight /= theta_x.shape[-1]**0.5
-        pairwise_weight = pairwise_weight.softmax(dim=-1)
-        return pairwise_weight
-
-    def dot_product(self, theta_x, phi_x):
-        # pairwise_weight: [N, HxW, HxW]
-        pairwise_weight = torch.matmul(theta_x, phi_x)
-        pairwise_weight /= pairwise_weight.shape[-1]
-        return pairwise_weight
-
-    def forward(self, x, x_ref):
-        """ g is ref value, phi is ref key, theta is current(query)"""
-        n, _, h, w = x.shape
-
-        # g_x: [N, HxW, C]
-        g_x = self.g(x_ref).view(n, self.inter_channels, -1)
-        g_x = g_x.permute(0, 2, 1)
-
-        # theta_x: [N, HxW, C]
-        theta_x = self.theta(x).view(n, self.inter_channels, -1)
-        theta_x = theta_x.permute(0, 2, 1)
-
-        # phi_x: [N, C, HxW]
-        phi_x = self.phi(x_ref).view(n, self.inter_channels, -1)
-
-        pairwise_func = getattr(self, self.mode)
-        # pairwise_weight: [N, HxW, HxW]
-        pairwise_weight = pairwise_func(theta_x, phi_x)
-
-        # y: [N, HxW, C]
-        y = torch.matmul(pairwise_weight, g_x)
-        # y: [N, C, H, W]
-        y = y.permute(0, 2, 1).reshape(n, self.inter_channels, h, w)
-
-        output = x + self.conv_out(y)
-
-        return output
-
-
 class CA(nn.Module):
 
     def __init__(self,
@@ -126,7 +16,8 @@ class CA(nn.Module):
                  use_scale=True,
                  conv_cfg=None,
                  norm_cfg=None,
-                 mode='embedded_gaussian'):
+                 mode='embedded_gaussian',
+                 conv_final=True):
         super(CA, self).__init__()
         self.in_channels = in_channels
         self.reduction = reduction
@@ -163,7 +54,40 @@ class CA(nn.Module):
             norm_cfg=norm_cfg,
             activation=None)
 
-        self.init_weights()
+        if conv_final:
+            self.conv_final = nn.Sequential(
+                ConvModule(
+                    in_channels=2*in_channels,
+                    out_channels=256,
+                    kernel_size=1,
+                    padding=0,
+                    stride=1,
+                    activation='relu'),
+                ConvModule(
+                    in_channels=256,
+                    out_channels=16,
+                    kernel_size=3,
+                    padding=1,
+                    stride=1,
+                    activation='relu'),
+                ConvModule(
+                    in_channels=16,
+                    out_channels=3,
+                    kernel_size=3,
+                    padding=1,
+                    stride=1),
+                nn.Conv2d(
+                    in_channels=3,
+                    out_channels=1,
+                    kernel_size=3,
+                    padding=1,
+                    stride=1)
+            )
+
+        if self.conv_final is not None:
+            self.init_weights(zeros_init=False)
+        else:
+            self.init_weights(zeros_init=True)
 
     def init_weights(self, std=0.01, zeros_init=True):
         for m in [self.g, self.theta, self.phi]:
@@ -172,6 +96,8 @@ class CA(nn.Module):
             constant_init(self.conv_out.conv, 0)
         else:
             normal_init(self.conv_out.conv, std=std)
+        if self.conv_final is not None:
+            nn.init.constant_(self.conv_final[-1].bias, 0.0)
 
     def embedded_gaussian(self, theta_x, phi_x):
         # pairwise_weight: [N, DxD, H, W]
@@ -203,7 +129,12 @@ class CA(nn.Module):
 
         y = self.assemble(pairwise_weight, g_x)
 
-        output = x + self.conv_out(y)
+        if self.conv_final is not None:
+            y = self.conv_out(y)
+            cat_feat = torch.cat([x, y], dim=1)
+            output = x + self.conv_final(cat_feat) * y
+        else:
+            output = x + self.conv_out(y)
 
         return output
 
@@ -211,30 +142,42 @@ class CA(nn.Module):
 @PAIR_MODULE.register_module
 class TwinEmbedCA(nn.Module):
 
-    def __init__(self, channels=256, reduction=2):
+    def __init__(self, channels=256, reduction=2, conv_final=True):
         super(TwinEmbedCA, self).__init__()
-        kwargs = dict(use_scale=True, mode='embedded_gaussian')
+        kwargs = dict(
+            use_scale=True,
+            mode='embedded_gaussian',
+            conv_final=conv_final)
+        self.top_conv = ConvModule(
+            channels,
+            channels,
+            kernel_size=3,
+            padding=1,
+            stride=2)
         self.blocks = nn.ModuleList([
             CA(
                 in_channels=channels,
-                corr_disp=4,
+                corr_disp=1,
                 reduction=reduction,
                 **kwargs),
             CA(
                 in_channels=channels,
-                corr_disp=2,
+                corr_disp=1,
                 reduction=reduction,
                 **kwargs),
-            NonLocal2D(
+            CA(
                 in_channels=channels,
+                corr_disp=1,
                 reduction=reduction,
                 **kwargs),
-            NonLocal2D(
+            CA(
                 in_channels=channels,
+                corr_disp=1,
                 reduction=reduction,
                 **kwargs),
-            NonLocal2D(
+            CA(
                 in_channels=channels,
+                corr_disp=1,
                 reduction=reduction,
                 **kwargs),
         ])
@@ -242,6 +185,7 @@ class TwinEmbedCA(nn.Module):
     def init_weights(self):
         for g in self.blocks:
             g.init_weights()
+        self.top_conv.init_weights()
 
     def forward(self, feat, feat_ref, is_train=False):
         outs = [
@@ -249,6 +193,6 @@ class TwinEmbedCA(nn.Module):
             self.blocks[1](x=feat[1], x_ref=feat_ref[2]),
             self.blocks[2](x=feat[2], x_ref=feat_ref[3]),
             self.blocks[0](x=feat[3], x_ref=feat_ref[4]),
-            self.blocks[1](x=feat[4], x_ref=feat[4]),
+            self.blocks[1](x=feat[4], x_ref=self.top_conv(feat_ref[4])),
         ]
         return outs
